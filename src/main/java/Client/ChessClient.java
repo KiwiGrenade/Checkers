8b2d553c6fd9c5f0df6d9ce169a8ebad561b7389
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2022-12-21 22:44:45 +0100

    Add mock server

--- src/main/java/Client/ChessClient.java
+++ src/main/java/Client/ChessClient.java
@@ -7,13 +7,11 @@ import java.util.Scanner;
 public class ChessClient {
     public static void main(String[] args) throws Exception {
         String serverAddress = "127.0.0.1";
-        try (Socket socket = new Socket(serverAddress, 59000)) {
+        try (Socket socket = new Socket(serverAddress, 59898)) {
             Scanner scanner = new Scanner(System.in);
-            Scanner input = new Scanner(socket.getInputStream());
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
             while (scanner.hasNextLine()) {
                 output.println(scanner.nextLine());
-                System.out.println(input.nextLine());
             }
         }
     }
---
+++ src/main/java/Server/ChessServer.java
@@ -0,0 +1,46 @@
+package Server;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ChessServer {
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket listener = new ServerSocket(59898)){
+            Executor pool = Executors.newFixedThreadPool(2);
+            while (true) {
+                pool.execute(new Game(listener.accept()));
+            }
+        }
+    }
+
+    private static class Game implements Runnable {
+        private Socket socket;
+
+        Game(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Connected: " + socket);
+            try {
+                Scanner input = new Scanner(socket.getInputStream());
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                while (input.hasNextLine()) {
+                    System.out.println(input.nextLine().toUpperCase());
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + socket);
+            } finally {
+                try {
+                    socket.close();
+                } catch (Exception e) {}
+                System.out.println("Closed: " + socket);
+            }
+        }
+    }
+}
