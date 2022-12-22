ae57d03a862ffe995a1d5186529602f9dfb0bbc9
RadzioTablecik <kpiech70@gmail.com>
2022-12-22 03:15:02 +0100

    tworzenie szkieletu

--- src/main/java/elo/demo/CheckersView.java
+++ src/main/java/elo/demo/CheckersView.java
@@ -1,17 +1,17 @@
 package elo.demo;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//baza pod widok
 public class CheckersView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CheckersView.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(new AnchorPane(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
