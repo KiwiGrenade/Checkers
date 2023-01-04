77ae0ba873693ad666c7f15691ad165d4b60f1fd
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2023-01-04 16:47:00 +0100

    Refactor class View.Tile and View.Pawn

--- src/main/java/View/Pawn.java
+++ src/main/java/View/Pawn.java
@@ -5,41 +5,18 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+import java.util.concurrent.SubmissionPublisher;
+
 // 2d representation of a pawn
 public class Pawn extends Circle {
     private boolean white;
     private int col;
     private int row;
 
-    public boolean isWhite() {
-        return white;
-    }
-
-    public void setWhite(boolean white) {
-        this.white = white;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public void setCol(int col) {
-        this.col = col;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public Pawn (boolean white, int col, int row) {
+    public Pawn (boolean white, int col, int row, double radius) {
+        super(radius, white ? Color.valueOf("#fff9f4") : Color.valueOf("#c40003"));
         this.white = white;
         this.col = col;
         this.row = row;
-        setRadius(CheckersController.TILE_SIZE / 2.0);
-        setFill(white ? Color.valueOf("#fff9f4") : Color.valueOf("#c40003"));
     }
 }
