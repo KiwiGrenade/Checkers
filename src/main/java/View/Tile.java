d55eb8b7700372538951818911c3f44b527c5fba
RadzioTablecik <kpiech70@gmail.com>
2023-01-07 12:17:20 +0100

    pola byly za male

--- src/main/java/View/Tile.java
+++ src/main/java/View/Tile.java
@@ -6,8 +6,8 @@ import javafx.scene.shape.Rectangle;
 //pole na szachownicy
 public class Tile extends Rectangle {
     public Tile(boolean white) {
-        super(CheckersController.TILE_SIZE / 2.0,
-                CheckersController.TILE_SIZE / 2.0,
+        super(CheckersController.TILE_SIZE,
+                CheckersController.TILE_SIZE,
                 white ? Color.valueOf("#feb") : Color.valueOf("#582")
         );
     }
