7577ed8c39fd5b605b656bab40f88286e1f86fa4
Mango T14 <kubajaskow@wp.pl>
2023-01-09 13:32:39 +0100

    Fix BlackQueen - not able to move

--- src/main/java/Server/BlackQueen.java
+++ src/main/java/Server/BlackQueen.java
@@ -5,39 +5,38 @@ public class BlackQueen extends Queen {
         super(x1, y1, 5);
     }
 
-
     @Override
     public boolean isPunchAvi(int x, int y, int color) {
-        return false;
+        return super.isPunchAvi(x, y, color);
     }
 
     @Override
     public boolean isPunchUpLeftAvi(int x, int y, int color) {
-        return false;
+        return super.isPunchUpLeftAvi(x, y, color);
     }
 
     @Override
     public boolean isPunchUpRightAvi(int x, int y, int color) {
-        return false;
+        return super.isPunchUpRightAvi(x, y, color);
     }
 
     @Override
     public boolean isPunchDownLeftAvi(int x, int y, int color) {
-        return false;
+        return super.isPunchDownLeftAvi(x, y, color);
     }
 
     @Override
     public boolean isPunchDownRightAvi(int x, int y, int color) {
-        return false;
+        return super.isPunchDownRightAvi(x, y, color);
     }
 
     @Override
     public boolean punch(int x2, int y2) {
-        return false;
+        return super.punch(x2, y2);
     }
 
     @Override
     public boolean normalMove(int x2, int y2) {
-        return false;
+        return super.normalMove(x2, y2);
     }
 }
